8bd7eb36116a91b3d516f1d49388483c64c796b4
Talshiar <domagoj.hrestak@gmail.com>
2015-12-14 16:22:53 +0100

    search implemented

--- BoyerMooreHorspoolCSharp/Program.cs
+++ BoyerMooreHorspoolCSharp/Program.cs
@@ -16,7 +16,7 @@ namespace BoyerMooreHorspoolCSharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StringSearch());
+            Application.Run(new StringSearch());            
         }
     }
 }
--- BoyerMooreHorspoolCSharp/StringSearch.cs
+++ BoyerMooreHorspoolCSharp/StringSearch.cs
@@ -13,6 +13,8 @@ namespace BoyerMooreHorspoolCSharp
 {
     public partial class StringSearch : Form
     {
+        bool usingFile = false;
+        string text = "";
         public StringSearch()
         {
             InitializeComponent();
@@ -28,10 +30,11 @@ namespace BoyerMooreHorspoolCSharp
                 string file = ofd.FileName;
                 try
                 {
-                    string text = File.ReadAllText(file);
+                    text = File.ReadAllText(file);
                     int size = text.Length;
                     textBoxFileText.Text = text;
                     groupBoxType.Enabled = false;
+                    usingFile = true;
                 }
                 catch(IOException)
                 {
@@ -43,12 +46,69 @@ namespace BoyerMooreHorspoolCSharp
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            inputDataCheck();
+            int index = search(text, textBoxPattern.Text);
+         
+        }
 
+        public int[] badCharactersTable(string pattern)
+        {
+            int[] table = new int[256];
+            for (int i = 0; i < 256; ++i)
+            {
+                table[i] = pattern.Length;
+            }
+            for (int i = 0; i < pattern.Length - 1; ++i)
+            {
+                table[pattern[i]] = pattern.Length - 1 - i;
+            }
+            return table;
+        }
+
+        public int search(string text, string pattern)
+        {
+            int[] badTable = badCharactersTable(textBoxPattern.Text);
+            int skip = 0;
+            int i;
+            while (text.Length - skip >= pattern.Length)
+            {
+                i = pattern.Length - 1;
+                while (text[skip + i] == pattern[i])
+                {
+                    if (i == 0) return skip;
+                    i = i - 1;
+                }
+                skip = skip + badTable[text[skip + pattern.Length - 1]];
+            }
+            return -1;
+        }
+        public void inputDataCheck()
+        {
+            if (!usingFile && textBoxInput.Text != "") text = textBoxInput.Text;
+            else if (!usingFile)
+            {
+                MessageBox.Show("Please browse for a text file or input random text to search through.");
+                return;
+            }
+            if (textBoxPattern.Text == "")
+            {
+                MessageBox.Show("Please enter a pattern you want to search for.");
+                return;
+            }
+            if (textBoxPattern.Text.Length > text.Length)
+            {
+                MessageBox.Show("The provided pattern is longer than the text you are searching through. Please write a shorter pattern.");
+            }
         }
 
         private void buttonReset_Click(object sender, EventArgs e)
         {
             Application.Restart();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
 
 
